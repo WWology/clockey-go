fc608958a899417a0b18431a1ddcad22d4c97b5b
WW <ww@wwology.tech>
2025-11-18 13:02:16 +0000

    Implement next command handler to retrieve and display upcoming game events

--- app/commands/next.go
+++ app/commands/next.go
@@ -1,7 +1,13 @@
 package commands
 
 import (
+	"cmp"
+	"fmt"
+	"slices"
+	"strings"
+
 	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/handler"
 )
 
 var Next = discord.SlashCommandCreate{
@@ -29,6 +35,35 @@ var Next = discord.SlashCommandCreate{
 					Value: "HoK",
 				},
 			},
+			Required: true,
 		},
 	},
+}
+
+func NextCommandHandler() handler.SlashCommandHandler {
+	return func(data discord.SlashCommandInteractionData, e *handler.CommandEvent) error {
+		game := data.String("game")
+		eventList, err := e.Client().Rest.GetGuildScheduledEvents(*e.GuildID(), false)
+		if err != nil {
+			return err
+		}
+
+		sortedEvents := slices.SortedStableFunc(slices.Values(eventList), func(a, b discord.GuildScheduledEvent) int {
+			return cmp.Compare(a.ScheduledStartTime.Unix(), b.ScheduledStartTime.Unix())
+		})
+
+		result := slices.IndexFunc(sortedEvents, func(event discord.GuildScheduledEvent) bool {
+			return strings.Contains(event.Name, game)
+		})
+
+		if result != -1 {
+			return e.CreateMessage(discord.MessageCreate{
+				Content: fmt.Sprintf("https://discord.com/events/%s/%s", e.GuildID(), sortedEvents[result].ID),
+			})
+		} else {
+			return e.CreateMessage(discord.MessageCreate{
+				Content: "No upcoming game found for " + game,
+			})
+		}
+	}
 }
