ee0988ba6cd689a38cbb35c1e2d3fbdf860573ef
WW <yosuamartiansia@gmail.com>
2025-12-15 11:05:53 +0000

    Enhance bot setup by adding GuildMembers intent and caching for members

--- app/app.go
+++ app/app.go
@@ -40,8 +40,8 @@ type Bot struct {
 
 func (b *Bot) SetupBot(listeners ...bot.EventListener) error {
 	client, err := disgo.New(b.Cfg.Bot.Token,
-		bot.WithGatewayConfigOpts(gateway.WithIntents(gateway.IntentGuilds, gateway.IntentGuildMessages, gateway.IntentMessageContent)),
-		bot.WithCacheConfigOpts(cache.WithCaches(cache.FlagGuilds)),
+		bot.WithGatewayConfigOpts(gateway.WithIntents(gateway.IntentGuilds, gateway.IntentGuildMessages, gateway.IntentMessageContent, gateway.IntentGuildMembers)),
+		bot.WithCacheConfigOpts(cache.WithCaches(cache.FlagGuilds|cache.FlagMembers)),
 		bot.WithEventListeners(listeners...),
 	)
 	if err != nil {
